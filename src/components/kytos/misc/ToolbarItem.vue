dfc77da219b6365d59c0a983dcb480eb77bd4372
Macartur Sousa <macartur.sc@gmail.com>
2018-04-11 16:59:17 -0300

    Add ToolbarItem.vue

---
+++ src/components/kytos/misc/ToolbarItem.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class='k-toolbar-item'  :icon="icon" :tooltip="tooltip">
+    <!-- @slot Slot with the toolbar item. -->
+    <slot>
+    </slot>
+ </div>
+</template>
+
+<script>
+import KytosBase from '../base/KytosBase'
+import KytosBaseWithIcon from '../base/KytosBaseWithIcon'
+
+export default {
+  name: 'k-toolbar-item',
+  mixins: [KytosBaseWithIcon],
+}
+
+</script>
