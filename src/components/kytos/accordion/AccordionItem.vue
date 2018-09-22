5afe15ce76a08ae8418f0b29f457dcce96aaa618
ValessioBrito <contato@valessiobrito.com.br>
2018-09-22 17:00:21 -0300

    Fix vars colors accordion

--- src/components/kytos/accordion/AccordionItem.vue
+++ src/components/kytos/accordion/AccordionItem.vue
@@ -55,7 +55,7 @@ export default {
   z-index: 99
 
 .tab:hover .k-accordion-label
- color: #DDD
+ color: $fill-icon-h
 
 .k-accordion-label
   position: relative
@@ -64,8 +64,8 @@ export default {
   margin-top: 5px
   cursor: pointer
   font-size: 0.7em
-  color: rgba(255,255,255,0.5)
-  border-bottom: 1px solid rgba(0,0,0,0.5)
+  color: $fill-icon
+  border-bottom: 1px solid $fill-bar
 
   &:after
     position: absolute
@@ -83,7 +83,7 @@ export default {
     position: relative
     margin-bottom: 1px
     width: 100%
-    color: rgba(255,255,255,0.8)
+    color: $fill-bar
     overflow: hidden
 
   .tab-content
@@ -93,7 +93,7 @@ export default {
     -webkit-transition: max-height .35s
     -o-transition: max-height .35s
     transition: max-height .35s
-    border-top: 1px solid rgba(255,255,255,0.05)
+    border-top: 1px solid $fill-panel-dark
 
     p
       margin: 1em
